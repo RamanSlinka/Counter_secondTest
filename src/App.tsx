d41c6eb91fcbcf3f08d4cf6973f7b7172ef8bf28
RamanSlinka <romuch1986@gmail.com>
2021-03-20 18:27:11 +0300

    counter2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <CounterSetting/>
+            <div></div>
+
+        </div>
+    );
 }
 
 export default App;
+
+ function CounterSetting() {
+    return (
+        <div>
+            <div>
+                <p>max value</p>
+                <input type="number"/>
+                <p>start value</p>
+                <input type="number"/>
+            </div>
+            <div>
+                <button>set</button>
+            </div>
+        </div>
+    )
+}
+export default  CounterSetting
