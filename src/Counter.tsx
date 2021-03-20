759fa20312f558d93579efb12ce8748af2627280
RamanSlinka <romuch1986@gmail.com>
2021-03-21 00:02:38 +0300

    added components

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -12,10 +12,10 @@ export function Counter(props: CounterPropsType) {
     return (
         <div className={'container'}>
             <div className={'setting-field'}>
-                <h2>{props.value}</h2>
+                <h2 className={props.value === 5 ? 'red' : ''}>{props.value}</h2>
             </div>
             <div className={'button-field'}>
-                <Button title={'inc'} onClick={props.increment} disabled={props.value === 5}/>
+                <Button title={'inc'} onClick={props.increment} disabled={props.value === 5} className={}/>
                 <Button title={'reset'}  onClick={props.resetValue} disabled={props.value < 5}/>
 
             </div>
